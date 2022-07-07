53b000ceffa2310a5c2d26ebcb814504901c5c5d
yaqub <105121706+Yaqub-naqeb@users.noreply.github.com>
2022-07-07 04:39:47 +0300

    Update Order.js

--- src/pages/Order.js
+++ src/pages/Order.js
@@ -1,11 +1,7 @@
 import React from "react";
 import OrderTitle from "../components/OrderTitle";
 import Card from "../components/Card";
-import { useState } from "react";
-
 export default function Order(props) {
-  const [data, setData] = useState([props.order]);
-
   return (
     <div className="flex p-10 justify-center flex-col items-center">
       <OrderTitle></OrderTitle>
@@ -13,10 +9,10 @@ export default function Order(props) {
       <div className="flex justify-center items-center flex-wrap p-10">
         {props.order.map((food) => (
           <Card
-            image={food.img}
+            image={food.image}
             nameFood={food.nameFood}
             price={food.price}
-            key={food}
+            key={food.idd}
           ></Card>
         ))}
       </div>
